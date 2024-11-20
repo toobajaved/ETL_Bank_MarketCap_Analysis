56f9a508423e293bd75e5937939ff9ef71f23cbd
Tooba <160694487+toobajaved@users.noreply.github.com>
2024-11-20 06:21:41 -0400

    Update etl_bankscript.py

--- etl_bankscript.py
+++ etl_bankscript.py
@@ -9,7 +9,7 @@
 # # Importing the required libraries
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd 
+import pandas as pd #df to store the data
 import sqlite3
 import numpy as np 
 from datetime import datetime
